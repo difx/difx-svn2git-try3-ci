e97f38a01db87af48c57801cd2e76afa03ce9941
HelgeRottmann <HelgeRottmann@example.com>
2008-09-10 14:53:35 +0000

    added svn properties to header
    svn path=/libraries/difxio/trunk/; revision=778

---
+++ libraries/difxio/difxio.h
@@ -0,0 +1,18 @@
+//===========================================================================
+// SVN properties (DO NOT CHANGE)
+//
+// $Id$
+// $HeadURL$
+// $LastChangedRevision$
+// $Author$
+// $LastChangedDate$
+//
+//============================================================================
+#ifndef DIFXIO_H
+#define DIFXIO_H
+
+#include "difxio/parsedifx.h"
+#include "difxio/parsevis.h"
+#include "difxio/difx_input.h"
+
+#endif
---
+++ libraries/mark5access/mark5access.h
@@ -0,0 +1,16 @@
+//===========================================================================
+// SVN properties (DO NOT CHANGE)
+//
+// $Id$
+// $HeadURL$
+// $LastChangedRevision$
+// $Author$
+// $LastChangedDate$
+//
+//============================================================================
+#ifndef MARK5ACCESS_H
+#define MARK5ACCESS_H
+
+#include "mark5access/mark5_stream.h"
+
+#endif
