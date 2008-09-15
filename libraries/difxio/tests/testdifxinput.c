61cdd6c673fe5308719304da461425af72aeca17
WalterBrisken <WalterBrisken@example.com>
2008-09-15 13:48:56 +0000

    update properties, README
    svn path=/libraries/difxio/trunk/; revision=797

--- libraries/difxio/tests/testdifxinput.c
+++ libraries/difxio/tests/testdifxinput.c
@@ -20,17 +20,46 @@
  * SVN properties (DO NOT CHANGE)
  *
  * $Id$
- * $HeadURL$
+ * $HeadURL: https://svn.atnf.csiro.au/difx/libraries/mark5access/trunk/mark5access/mark5_stream.c $
  * $LastChangedRevision$
  * $Author$
  * $LastChangedDate$
  *
  *==========================================================================*/
-#ifndef DIFXIO_H
-#define DIFXIO_H
 
-#include "difxio/parsedifx.h"
-#include "difxio/parsevis.h"
-#include "difxio/difx_input.h"
+#include <stdio.h>
+#include "difx_input.h"
 
-#endif
+int main(int argc, char **argv)
+{
+	DifxInput *D;
+	
+	if(argc < 2)
+	{
+		printf("Usage : %s <inputfilebase>\n", argv[0]);
+		return 0;
+	}
+	
+	D = loadDifxInput(argv[1]);
+	if(!D)
+	{
+		fprintf(stderr, "D == 0.  quitting\n");
+		return 0;
+	}
+
+	D = updateDifxInput(D);
+	if(!D)
+	{
+		fprintf(stderr, "update failed: D == 0.  quitting\n");
+		return 0;
+	}
+
+	printDifxInput(D);
+
+	writeDifxCalc(D, "calc.out");
+	writeDifxInput(D, "input.out");
+
+	deleteDifxInput(D);
+
+	return 0;
+}
